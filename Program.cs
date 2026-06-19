91518b64c64c71caa139bc8f334ec10874f254c2
Nicholas <nicholaskm254@gmail.com>
2026-06-19 12:15:19 +0300

    fix CORS issue

--- Program.cs
+++ Program.cs
@@ -3,31 +3,49 @@ using InventoryApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// services
+// SERVICES
 builder.Services.AddControllers();
+
+// 🔥 CORS (ADD THIS)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+// 🔥 AUTO MIGRATION
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
 
-// Your endpoints
+// TEST ENDPOINT
 app.MapGet("/di-test", (AppDbContext db) =>
 {
     return Results.Ok("DI WORKS");
 });
 
-
+// 🔥 SWAGGER
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// 🔥 CORS MUST BE HERE (IMPORTANT ORDER)
+app.UseCors("AllowFrontend");
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
