be750b77abb5a89977cc943ddb77dad6d3d08ac3
Nicholas <nicholaskm254@gmail.com>
2026-06-19 10:56:43 +0300

    Fix PostgreSQL migrations again

--- Migrations/20260619075522_InitialCreate.Designer.cs
+++ Migrations/20260619075522_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260619074330_InitialCreate")]
+    [Migration("20260619075522_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
